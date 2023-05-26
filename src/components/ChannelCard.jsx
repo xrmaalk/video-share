1afc3ff36ea1fd9e17a6e234d0a510821190439c
xrmaalk <xinra.inc@gmail.com>
2023-05-25 21:35:55 -0600

    VideoCard component update
    CardContent add

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,4 +1,18 @@
-import React from "react";
+// eslint-disable-next-line
+import { Link } from "react-router-dom";
+// eslint-disable-next-line
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+// eslint-disable-next-line
+import { CheckCircle } from "@mui/icons-material";
+
+import {
+  // eslint-disable-next-line
+  demoThumbnailUrl,
+  // demoVideoUrl,
+  // demoVideoTitle,
+  // demoChannelUrl,
+  // demoChannelTitle,
+} from "../utils/constants";
 
 const ChannelCard = () => {
   return <div>ChannelCard</div>;
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { CheckCircle } from "@mui/icons-material";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+import {
+  // eslint-disable-next-line
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  console.log(videoId, snippet);
+  return (
+    <Card sx={{ borderRadius: "50px" }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 555, height: 275 }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "55px" }}>
+        <Grid container direction="column">
+          <Grid item>
+            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+              <Typography variant="subtitle1" fontWeight="bold" color="#fff">
+                {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+              </Typography>
+            </Link>
+          </Grid>
+          <Grid item sx={{ mt: "10px" }}>
+            <Link
+              to={
+                snippet?.channelId
+                  ? `/channel/${snippet?.channelId}`
+                  : demoChannelUrl
+              }
+            >
+              <Typography variant="subtitle2" fontWeight="bold" color="gray">
+                {snippet?.channelTitle || demoChannelTitle}
+                <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+              </Typography>
+            </Link>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default VideoCard;
