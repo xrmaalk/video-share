1a44b9e9620e0271350f78c7f33feb4fb503626e
xrmaalk <xinra.inc@gmail.com>
2023-05-26 11:45:25 -0600

    Card style tweaks

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,21 +1,63 @@
 // eslint-disable-next-line
 import { Link } from "react-router-dom";
 // eslint-disable-next-line
-import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { Box, Typography, Card, CardContent, CardMedia } from "@mui/material";
 // eslint-disable-next-line
 import { CheckCircle } from "@mui/icons-material";
+// eslint-disable-next-line
+import { demoProfilePicture } from "../utils/constants";
 
-import {
-  // eslint-disable-next-line
-  demoThumbnailUrl,
-  // demoVideoUrl,
-  // demoVideoTitle,
-  // demoChannelUrl,
-  // demoChannelTitle,
-} from "../utils/constants";
+const ChannelCard = ({ channelDetail }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "100%", md: "555px" },
+      height: "326px",
+      margin: "auto",
+    }}
+  >
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "#fff",
+        }}
+      >
+        <CardMedia
+          image={
+            channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
+          }
+          alt={channelDetail?.snippet?.title}
+          sx={{
+            borderRadius: "50%",
+            height: "180px",
+            width: "180px",
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
-};
+            border: ".25rem solid #e3e3e3",
+          }}
+        />
+        <Typography variant="h6" sx={{ mt: "1rem" }}>
+          {channelDetail?.snippet?.title}
+          <CheckCircle sx={{ fontSize: 16, color: "#1e1e", ml: "5px" }} />
+        </Typography>
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography variant="h6" sx={{ mt: "1rem", ml: "2rem" }}>
+            {parseInt(
+              channelDetail?.statistics?.subscriberCount
+            ).toLocaleString()}
+            subscribers
+          </Typography>
+        )}
+      </CardContent>
+    </Link>
+  </Box>
+);
 
 export default ChannelCard;
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -20,7 +20,13 @@ const VideoCard = ({
 }) => {
   console.log(videoId, snippet);
   return (
-    <Card sx={{ borderRadius: "50px" }}>
+    <Card
+      sx={{
+        width: { md: "555px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: "50px",
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
@@ -33,7 +39,7 @@ const VideoCard = ({
           <Grid item>
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
               <Typography variant="subtitle1" fontWeight="bold" color="#fff">
-                {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+                {snippet?.title.slice(0, 35) || demoVideoTitle.slice(0, 60)}
               </Typography>
             </Link>
           </Grid>
@@ -47,7 +53,7 @@ const VideoCard = ({
             >
               <Typography variant="subtitle2" fontWeight="bold" color="gray">
                 {snippet?.channelTitle || demoChannelTitle}
-                <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+                <CheckCircle sx={{ fontSize: 12, color: "#1e1e", ml: "5px" }} />
               </Typography>
             </Link>
           </Grid>
