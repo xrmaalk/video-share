1e0bf2950e7e78b2a57ffd7ed14e785866d5aea4
xrmaalk <xinra.inc@gmail.com>
2023-05-27 14:16:24 -0600

    README
    complete

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -31,7 +31,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright 2023 KaBOOM
+          Copyright 2023 vidSearch
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
